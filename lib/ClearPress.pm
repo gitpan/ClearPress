ee932d97687c1b61c44d9be64a8560c8932f4f4f
Roger Pettett <rmp@psyphi.net>
2010-01-04 14:16:18 +0000

    Import of RPETTETT/ClearPress-348 from CPAN.
    gitpan-cpan-distribution: ClearPress
    gitpan-cpan-version:      348
    gitpan-cpan-path:         RPETTETT/ClearPress-348.tar.gz
    gitpan-cpan-author:       RPETTETT
    gitpan-cpan-maturity:     released

--- lib/ClearPress.pm
+++ lib/ClearPress.pm
@@ -13,7 +13,7 @@ use ClearPress::view;
 use ClearPress::controller;
 use ClearPress::util;
 
-our $VERSION = do { my ($r) = q$Revision: 318 $ =~ /(\d+)/smx; $r; };
+our $VERSION = do { my ($r) = q$Revision: 348 $ =~ /(\d+)/smx; $r; };
 
 1;
 __END__
